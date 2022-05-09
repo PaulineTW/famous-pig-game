c584cb24eccf3fbd86e4ef0d38e8124858211c6a
pauline rouger <paulinerouge@hotmail.com>
2022-05-09 10:21:48 +0800

    Add class hidden

--- script.js
+++ script.js
@@ -3,9 +3,9 @@
 // Selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1'); // same as querySelector
+const diceEl = document.querySelector('.dice')
 
-
+// Starting condition
 score0El.textContent = 0;
 score1El.textContent = 0;
-
-console.log(score0El);
+diceEl.classList.add('hidden')
