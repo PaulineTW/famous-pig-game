535abd96f16f88203953da4060d71859b45c0c8d
pauline rouger <paulinerouge@hotmail.com>
2022-05-09 10:58:40 +0800

    Set current score and Switch to next player

--- script.js
+++ script.js
@@ -17,23 +17,28 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0] // start with 0 for both players
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 
 btnRoll.addEventListener('click', function () {
 // 1. Generate a random dice roll
 const dice = Math.trunc(Math.random() * 6) + 1;
+console.log(dice);
 // 2. Display dice
 diceEl.classList.remove('hidden');
 diceEl.src = `dice-${dice}.png`;
 // 3. Check for rolled 1: if true, switch player
  if(dice !== 1) {
    // Add dice to current score
-  currentScore += dice;
-  current0El.textContent = currentScore;
-
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
  } else {
-
+   // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
  }
 })
