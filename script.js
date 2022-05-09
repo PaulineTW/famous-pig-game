a15d95d312f024bb658c5a02524eee702b7ca783
pauline rouger <paulinerouge@hotmail.com>
2022-05-09 11:35:53 +0800

    Stop the game functionality

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ diceEl.classList.add('hidden');
 const scores = [0, 0] // start with 0 for both players
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -33,42 +34,45 @@ const switchPlayer = function () {
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
-// 1. Generate a random dice roll
-const dice = Math.trunc(Math.random() * 6) + 1;
-console.log(dice);
-// 2. Display dice
-diceEl.classList.remove('hidden');
-diceEl.src = `dice-${dice}.png`;
-// 3. Check for rolled 1: if true, switch player
- if(dice !== 1) {
-  // Add dice to current score
-  currentScore += dice;
-  document.getElementById(`current--${activePlayer}`).textContent = currentScore;
- } else {
-  // Switch to next player
-  switchPlayer();
+  if(playing) {
+  // 1. Generate a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  // 3. Check for rolled 1: if true, switch player
+  if(dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+  } else {
+    // Switch to next player
+    switchPlayer();
+  }
  }
 })
 
 // Hold current score
 btnHold.addEventListener('click', function() {
-  // 1. Add current score to actice player's score
-  scores[activePlayer] += currentScore;
-  // scores[1] = scores[1] + currentScore
-  document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
-  // 2. Check if player's score is 100
-  if (score[activePlayer] <= 100) {
-    // Finish the game
-    document
-      .querySelector(`player--${activePlayer}`)
+  if(playing) {
+    // 1. Add current score to actice player's score
+    scores[activePlayer] += currentScore;
+    // scores[1] = scores[1] + currentScore
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+    // 2. Check if player's score is 100
+    if (scores[activePlayer] >= 20) {
+      // Finish the game
+      playing = false;
+      document
+      .querySelector(`.player--${activePlayer}`)
       .classList.add('player--winner');
-    document
-      .querySelector(`player--${activePlayer}`)
-      .classList.add('player--active');
-
-  } else {
-
+      document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove('player--active');
+    } else {
+      // 3. Switch to next player
+      switchPlayer();
+    }
   }
-  // 3. Switch to next player
-  switchPlayer();
-})
+});
